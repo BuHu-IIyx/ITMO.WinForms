0c504e50ea264e82e49f307670f261c6e91723cd
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-06-20 17:36:45 +0300

    20/06/2021

---
+++ ITMO.WinForms.Lab2.Ex6.BiblWorm/Bibl/IPubs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITMO.WinForms.Lab2.Ex6.BiblWorm
+{
+    interface IPubs
+    {
+        
+       void Subs ();
+                
+    }
+}
